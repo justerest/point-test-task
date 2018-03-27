f32c0744ff3c94607115fd94b152f20a74fb34b2
Sergey Klevakin <justerest@yandex.ru>
2018-03-27 16:21:36 +0500

    feat(news): add creation form

--- src/app/core/news/news-create.component.ts
+++ src/app/core/news/news-create.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { News } from '../../news.model';
+import { EventsService } from '../../events.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-news-create',
@@ -11,9 +13,16 @@ export class NewsCreateComponent implements OnInit {
 
   news = new News;
 
-  constructor() { }
+  constructor(
+    private eventsService: EventsService,
+    private router: Router,
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  public save() {
+    this.eventsService.create(this.news);
+    this.router.navigate(['/events']);
   }
 
 }
